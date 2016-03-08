57edf3bd6b0b947a719abde6283475ef8b0ba85a
amandine19 <amandinevabre@yahoo.fr>
2016-03-08 17:24:18 +0100

    upload model page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,8 @@
 class Page < ActiveRecord::Base
 
   	
-  has_attached_file :image, styles: { medium: "150x150>", thumb: "60x60>"}	
-  validates_attachment :image, content_type: [ 'image/png', 'image/jpg', 'image/gif', "application/pdf", 
+  has_attached_file :table_uploads, styles: { medium: "150x150>", thumb: "60x60>"}	
+  validates_attachment :type, content_type: [ 'type/png', 'jpg', 'image/gif', "application/pdf", 
                                    						'video/mpeg', 'video/quicktime', 'video/x-ms-asf', 'video/x-msvideo', 
                                  						'video/x-flv' ]
   #validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
@@ -13,5 +13,5 @@ class Page < ActiveRecord::Base
   validates :name,			:presence => true, length: { maximum: 250 }
   validates :user_id, 		:presence => true
   validates :container_id, 	:presence => true
-  validates :image_id,      :presence => true
+  validates :upload_id,     :presence => true
 end
