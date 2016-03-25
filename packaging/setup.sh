44f59503020e6cf5325ffb219687fc093e40193a
Pierre Duverneix <pierre.duverneix@gmail.com>
2016-03-25 17:58:08 +0100

    setup

--- packaging/setup.sh
+++ packaging/setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ffmpeg -L foo >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed. Aborting."; exit 1; }
+git --version foo >/dev/null 2>&1 || { 
+	echo >&2 "Git missing.";
+	sudo bash -c 'apt-get -y install git >/dev/null 2>&1 & disown'
+}
+
+ffmpeg -L foo >/dev/null 2>&1 || { 
+	echo >&2 "FFMPEG missing.";
+	git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
+	ffmpeg/configure && make && make install
+}
