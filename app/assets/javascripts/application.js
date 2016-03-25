4a02d8604c50439055404d4338bf941f8da9a913
Thiéfaine <yaourtnut@gmail.com>
2016-03-25 14:26:18 +0100

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,13 @@
 
 //= require jquery.sortable.min
 
+//= require bootstrap/dist/js/bootstrap.min.js
+//= require sb-admin-2
+//= require metisMenu.min
+
 //= require tinymce/tinymce.min.js
 //= require tinymceplugins/uploader.tinymce.plugin
 //= require tinymceplugins/formula.tinymce.plugin
 //= require tinymceplugins/glossary.tinymce.plugin
 
 //= require all
-
